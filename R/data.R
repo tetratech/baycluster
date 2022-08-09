13f74472d659e3202d37d933b9d36bc537f63feb
Jon Harcum <jon.harcum@tetratech.com>
2022-08-09 16:31:14 -0400

    v0.0.1.9007

--- R/data.R
+++ R/data.R
@@ -35,3 +35,40 @@
 #'   \item{group}{group}
 #' }
 "shp_cbseg"
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# cbp_stat_list ####
+#' @title Station vectors for selections portions of Chesapeake Bay
+#' tidal waters.
+#' 
+#' @description Station vectors for selections portions of Chesapeake Bay
+#' tidal waters.
+#' 
+#' @format A list with the following variables:
+#' \describe{
+#'   \item{summary}{Summary table of abbreviations, descriptions and stations}            
+#'   \item{cbp}{Main Bay Stations}            
+#'   \item{u_cbp}{Upper Bay Stations}           
+#'   \item{md_cbp}{MD Bay Stations}              
+#'   \item{va_cbp}{VA Bay Stations}              
+#'   \item{md_wt}{MD Western Tributary Stations}
+#'   \item{md_et}{MD Eastern Tributary Stations}
+#'   \item{va_we}{VA Western Embayment Stations}
+#'   \item{va_ee}{VA Eastern Embayment Stations}
+#'   \item{pax}{Patuxent Stations}            
+#'   \item{pot}{Potomac Stations}             
+#'   \item{rpp}{Rappahannock Stations}        
+#'   \item{yrk}{York Stations}                
+#'   \item{jms}{James Stations}               
+#'   \item{ebe}{Elizabeth River Stations} 
+#' }
+"cbp_stat_list"
+
+
+
+
+
+
+
+
